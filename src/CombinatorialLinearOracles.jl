df73cac4b69e8b965ce90a4567d9cd7067498393
Victor <victor.thouvenot@gmail.com>
2024-06-18 15:50:41 +0200

    first version of matching and spanning tree oracle

--- src/CombinatorialLinearOracles.jl
+++ src/CombinatorialLinearOracles.jl
@@ -1,5 +1,75 @@
 module CombinatorialLinearOracles
 
-# Write your package code here.
+import FrankWolfe
+using Graphs
+using SparseArrays
+using GraphsMatching
+
+"""
+Spanning Tree LMO
+"""
+struct SpanningTreeLMO{G} <: FrankWolfe.LinearMinimizationOracle
+    graph::G
+end
+
+function compute_extreme_point(
+    lmo::SpanningTreeLMO,
+    direction::M;
+    v=nothing,
+    kwargs...,
+) where {M}
+    N = length(direction)
+    iter = collect(Graphs.edges(lmo.graph))
+    distmx = spzeros(N,N)
+    for idx in 1:N
+        if(direction[idx] > 0)
+            distmx[src(iter[idx]),dst(iter[idx])] = direction[idx]
+            distmx[dst(iter[idx]),src(iter[idx])] = direction[idx]
+        end
+    end
+    span = Graphs.kruskal_mst(lmo.graph,distmx)
+    v = spzeros(N)
+    for edge in span
+        for i in 1:N
+            if(src(edge) == src(iter[i]) && dst(edge) == dst(iter[i]))
+                v[i] = 1
+            end
+        end
+    end
+    return v
+end
+
+
+"""
+Matching LMO
+"""
+struct MatchingLMO{G} <: FrankWolfe.LinearMinimizationOracle
+    graph::G
+end
+
+function compute_extreme_point(
+    lmo::MatchingLMO,
+    direction::M;
+    v=nothing,
+    kwargs...,
+) where {M}
+    N = length(direction)
+    iter = collect(Graphs.edges(lmo.graph))
+    w = Dict{typeof(iter[1]),typeof(direction[1])}()
+    for i in N
+        w[iter[i]] = direction[i]
+    end
+    match = GraphsMatching.minimum_weight_perfect_matching(lmo.graph,w)
+    K = length(match.mate)
+    v = spzeros(K)
+    for i in 1:K
+        for edge in iter
+            if(math.mate[i] == src(edge) && dst(egde) == i)
+                v[i] = 1
+            end
+        end
+    end
+    return v
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
-using CombinatorialLinearOracles
+using .CombinatorialLinearOracles
 using Test
+using Random
+using Graphs
+using SparseArrays
 
-@testset "CombinatorialLinearOracles.jl" begin
-    # Write your tests here.
+@testset "Matching LMO" begin
+    N = Int(1e1)
+    Random.seed!(4321)
+    g = Graphs.complete_graph(N)
+    iter = collect(Graphs.edges(g))
+    M = length(iter)
+    direction = randn(M)
+    lmo = CombinatorialLinearOracles.MatchingLMO(g)
+    v = CombinatorialLinearOracles.compute_extreme_point(lmo,direction)
+    tab = zeros(M)
+    is_matching = true
+    for i in 1:M
+        if(v[i] == 1)
+            is_matching = (tab[src(iter[i])] == 0 && tab[dst(iter[i])] == 0)
+            if(!is_matching)
+                break
+            end
+            tab[src(iter[i])] = 1
+            tab[dst(iter[i])] = 1
+        end    
+    end
+    @test is_matching == true
 end
